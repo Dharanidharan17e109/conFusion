59d10e6b5a94b0c06b4528f9752b309eb6ccc778
Dharanidharan <dharanidharansivasamy@gmail.com>
2020-08-28 18:12:43 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+import{ DishService } from './services/dish.service';
+
+
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-import { MatListModule } from '@angular/material/list'
+import { MenuComponent } from './menu/menu.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +47,12 @@ import { MatListModule } from '@angular/material/list'
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DishService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import{ Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
-import { DISHES } from '../shared/dishes';
+
 
 
 @Component({
@@ -12,14 +13,15 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
+  dishes:Dish[];
 
   selectedDish:Dish;
 
 
-  constructor() { }
+  constructor(private dishService:DishService) { }
 
   ngOnInit(): void {
+    this.dishes=this.dishService.getDishes();
   }
 
   onSelect(dish:Dish){
